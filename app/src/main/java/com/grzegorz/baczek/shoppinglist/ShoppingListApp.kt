64d55099f7fa1f1bee25f05a38bf3e4661d3329d
Grzegorz Bączek <grzegorz.baczek.93@gmail.com>
2022-08-28 11:20:50 +0200

    Integrated compose navigation and koin

---
+++ app/src/main/java/com/grzegorz/baczek/shoppinglist/ShoppingListApp.kt
@@ -0,0 +1,22 @@
+package com.grzegorz.baczek.shoppinglist
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class ShoppingListApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@ShoppingListApp)
+            modules()
+        }
+    }
+}
