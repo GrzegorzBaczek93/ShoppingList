12a0bac1ccf67b34a6fc9cb3d6ebb0506b09c300
Grzegorz Baczek <grzegorz.baczek.93@gmail.com>
2022-10-10 21:54:12 +0200

    Refactored Screen class -> added composable

--- app/src/main/java/com/grzegorz/baczek/shoppinglist/navigation/Navigation.kt
+++ app/src/main/java/com/grzegorz/baczek/shoppinglist/navigation/Navigation.kt
@@ -2,8 +2,6 @@ package com.grzegorz.baczek.shoppinglist.navigation
 
 import androidx.compose.runtime.Composable
 import com.grzegorz.baczek.shoppinglist.ui.screen.home.HomeArguments
-import com.grzegorz.baczek.shoppinglist.ui.screen.home.compose.HomeScreen
-import com.grzegorz.baczek.shoppinglist.ui.screen.list.compose.ListScreen
 import dev.olshevski.navigation.reimagined.NavBackHandler
 import dev.olshevski.navigation.reimagined.NavHost
 import dev.olshevski.navigation.reimagined.rememberNavController
@@ -14,10 +12,5 @@ fun Navigation() {
 
     NavBackHandler(controller = navHostController)
 
-    NavHost(navHostController) { screen ->
-        when (screen) {
-            is Screen.Home -> HomeScreen(navHostController, screen.arguments)
-            is Screen.List -> ListScreen(navHostController, screen.arguments)
-        }
-    }
+    NavHost(navHostController) { screen -> screen.Content(navHostController) }
 }
--- app/src/main/java/com/grzegorz/baczek/shoppinglist/navigation/Screen.kt
+++ app/src/main/java/com/grzegorz/baczek/shoppinglist/navigation/Screen.kt
@@ -1,18 +1,35 @@
 package com.grzegorz.baczek.shoppinglist.navigation
 
 import android.os.Parcelable
+import androidx.compose.runtime.Composable
 import com.grzegorz.baczek.shoppinglist.ui.screen.home.HomeArguments
+import com.grzegorz.baczek.shoppinglist.ui.screen.home.compose.HomeScreen
 import com.grzegorz.baczek.shoppinglist.ui.screen.list.ListArguments
+import com.grzegorz.baczek.shoppinglist.ui.screen.list.compose.ListScreen
 import com.grzegorz.baczek.shoppinglist.utils.base.BaseArguments
+import dev.olshevski.navigation.reimagined.NavController
 import kotlinx.parcelize.Parcelize
 
 sealed class Screen(
     open val arguments: BaseArguments
 ) : Parcelable {
 
-    @Parcelize
-    data class Home(override val arguments: HomeArguments) : Screen(arguments)
+    @Composable
+    open fun Content(navController: NavController<Screen>) {}
 
     @Parcelize
-    data class List(override val arguments: ListArguments) : Screen(arguments)
+    data class Home(override val arguments: HomeArguments) : Screen(arguments) {
+        @Composable
+        override fun Content(navController: NavController<Screen>) {
+            HomeScreen(navController, arguments)
+        }
+    }
+
+    @Parcelize
+    data class List(override val arguments: ListArguments) : Screen(arguments) {
+        @Composable
+        override fun Content(navController: NavController<Screen>) {
+            ListScreen(navController, arguments)
+        }
+    }
 }
