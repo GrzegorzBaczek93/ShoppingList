1240addabeeb68e6fcaf7f9c8f7df4d9d7c02ff2
Grzegorz Baczek <grzegorz.baczek.93@gmail.com>
2022-10-23 22:00:01 +0200

    Moved app bar title to start, fixed text overflow

--- app/src/main/java/com/grzegorz/baczek/shoppinglist/ui/component/common/AppBar.kt
+++ app/src/main/java/com/grzegorz/baczek/shoppinglist/ui/component/common/AppBar.kt
@@ -3,22 +3,26 @@ package com.grzegorz.baczek.shoppinglist.ui.component.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.grzegorz.baczek.shoppinglist.R
 
 private val dimens = object {
     val height = 64.dp
     val horizontalPadding = 4.dp
+    val startContainerMinWidth = 12.dp
 }
 
 @Composable
@@ -28,23 +32,46 @@ fun AppBar(
     startContainer: @Composable () -> Unit = {},
     endContainer: @Composable () -> Unit = {},
 ) {
-    Box(
+    ConstraintLayout(
         modifier = modifier
             .background(MaterialTheme.colorScheme.surface)
             .fillMaxWidth()
             .height(dimens.height)
             .padding(horizontal = dimens.horizontalPadding),
     ) {
-        Box(modifier = Modifier.align(Alignment.CenterStart)) {
+        val (startContainerRef, endContainerRef, titleContainer) = createRefs()
+        Box(
+            modifier = Modifier
+                .defaultMinSize(minWidth = dimens.startContainerMinWidth)
+                .constrainAs(startContainerRef) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                },
+        ) {
             startContainer()
         }
         Text(
-            modifier = Modifier.align(Alignment.Center),
+            modifier = Modifier.constrainAs(titleContainer) {
+                    start.linkTo(startContainerRef.end)
+                    end.linkTo(endContainerRef.start)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    width = Dimension.fillToConstraints
+                },
             text = title,
             style = MaterialTheme.typography.titleLarge,
             color = MaterialTheme.colorScheme.onSurface,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
         )
-        Row(modifier = Modifier.align(Alignment.CenterEnd)) {
+        Row(
+            modifier = Modifier.constrainAs(endContainerRef) {
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            },
+        ) {
             endContainer()
         }
     }
@@ -52,6 +79,19 @@ fun AppBar(
 
 @Preview
 @Composable
-private fun SearchBarPreview() {
-    AppBar()
+private fun SearchBarCenterPreview() {
+    AppBar(
+        title = "Very long title with even longer text",
+        endContainer = { AppBarButton(onClick = {}, imageDrawable = R.drawable.ic_delete) },
+    )
+}
+
+@Preview
+@Composable
+private fun SearchBarStartPreview() {
+    AppBar(
+        title = "Very long title with even longer text",
+        startContainer = { AppBarButton(onClick = {}, imageDrawable = R.drawable.ic_arrow_back) },
+        endContainer = { AppBarButton(onClick = {}, imageDrawable = R.drawable.ic_delete) },
+    )
 }
