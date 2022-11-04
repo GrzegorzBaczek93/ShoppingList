765a4d27345af2c41abfccff103fc54a21bdd8a5
Grzegorz Baczek <grzegorz.baczek.93@gmail.com>
2022-11-04 15:28:21 +0100

    Implemented dialogs handling

--- app/src/main/java/com/grzegorz/baczek/shoppinglist/ui/navigation/Navigation.kt
+++ app/src/main/java/com/grzegorz/baczek/shoppinglist/ui/navigation/Navigation.kt
@@ -2,14 +2,18 @@ package com.grzegorz.baczek.shoppinglist.ui.navigation
 
 import android.os.Bundle
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import dev.olshevski.navigation.reimagined.DialogNavHost
 import dev.olshevski.navigation.reimagined.NavHost
 import org.koin.androidx.compose.getStateViewModel
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigation() {
     val viewModel = getStateViewModel<NavigationViewModel>(state = { Bundle.EMPTY })
 
     BackHandler(enabled = viewModel.backEnabled, onBack = viewModel::onBackPress)
     NavHost(backstack = viewModel.backStack) { screen -> screen.Content() }
+    DialogNavHost(backstack = viewModel.dialogBackStack) { dialog -> dialog.Content() }
 }
--- app/src/main/java/com/grzegorz/baczek/shoppinglist/ui/navigation/NavigationViewModel.kt
+++ app/src/main/java/com/grzegorz/baczek/shoppinglist/ui/navigation/NavigationViewModel.kt
@@ -8,12 +8,14 @@ import androidx.lifecycle.viewmodel.compose.saveable
 import com.grzegorz.baczek.shoppinglist.navigation.action.NavigationAction
 import com.grzegorz.baczek.shoppinglist.navigation.action.NavigationHandler
 import com.grzegorz.baczek.shoppinglist.navigation.destination.Destination
+import com.grzegorz.baczek.shoppinglist.navigation.destination.Dialog
 import com.grzegorz.baczek.shoppinglist.navigation.destination.Screen
 import com.grzegorz.baczek.shoppinglist.ui.screen.home.HomeArguments
 import dev.olshevski.navigation.reimagined.NavController
 import dev.olshevski.navigation.reimagined.navController
 import dev.olshevski.navigation.reimagined.navigate
 import dev.olshevski.navigation.reimagined.pop
+import dev.olshevski.navigation.reimagined.popAll
 import dev.olshevski.navigation.reimagined.popUpTo
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -28,8 +30,14 @@ class NavigationViewModel(
         navController<Destination>(startDestination = Screen.Home(HomeArguments()))
     }
 
+    @OptIn(SavedStateHandleSaveableApi::class)
+    private val dialogNavController by savedStateHandle.saveable<NavController<Destination>> {
+        navController<Destination>(emptyList())
+    }
+
     val backStack get() = navController.backstack
     val backEnabled get() = navController.backstack.entries.size > 1
+    val dialogBackStack get() = dialogNavController.backstack
 
     init {
         navigationHandler.getActionFlow().onEach { handleAction(it) }.launchIn(viewModelScope)
@@ -41,10 +49,31 @@ class NavigationViewModel(
 
     private fun handleAction(action: NavigationAction) {
         when (action) {
-            is NavigationAction.NavigateTo -> navController.navigate(action.destination)
-            is NavigationAction.PopTo -> navController.popUpTo { it == action.destination }
-            NavigationAction.Pop -> navController.pop()
+            is NavigationAction.NavigateTo -> navigateTo(action.destination)
+            is NavigationAction.PopTo -> popTo(action.destination)
+            NavigationAction.Pop -> pop()
             NavigationAction.None -> {}
+        }
+    }
+
+    private fun navigateTo(destination: Destination) {
+        when (destination) {
+            is Screen -> navController.navigate(destination)
+            is Dialog -> dialogNavController.navigate(destination)
+        }
+    }
+
+    private fun popTo(destination: Destination) {
+        if (dialogBackStack.entries.isNotEmpty()) {
+            dialogNavController.popAll()
+        }
+        navController.popUpTo { it == destination }
+    }
+
+    private fun pop() {
+        when {
+            dialogBackStack.entries.isNotEmpty() -> dialogNavController.pop()
+            else -> navController.pop()
         }
     }
 }
